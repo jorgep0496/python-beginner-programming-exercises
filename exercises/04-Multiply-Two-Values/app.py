dddced6c1ae5eb7e6fa61ee40d446b43062068ab
jorgep0496 <jorgep0496@gmail.com>
2020-11-15 22:23:11 +0000

    python exercises

--- exercises/04-Multiply-Two-Values/app.py
+++ exercises/04-Multiply-Two-Values/app.py
@@ -1,1 +1,6 @@
 # your code here
+
+def variables_are_cool(a, b):
+    return a * b
+
+print(variables_are_cool(2345, 7323))
--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,6 @@
 import random
 
 # your code here
+generate_random = random.randint(0,9)
+
+print(generate_random)
